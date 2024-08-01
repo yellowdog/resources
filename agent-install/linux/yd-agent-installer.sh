a073e6553b3398e1c856b19b4f4ff0811f813b0c
Rupert Madden-Abbott <rupert.madden.abbott@yellowdog.co>
2024-08-01 11:50:16 +0100

    Remove all tasks now handled by deb/rpm.

--- agent-install/linux/yd-agent-installer.sh
+++ agent-install/linux/yd-agent-installer.sh
@@ -2,17 +2,11 @@
 
 # YellowDog Agent installer script.
 
-# Set "YD_INSTALL_JAVA" to anything other than "TRUE" to disable
-# installing Java. The Agent startup script will expect to find
-# a Java (v11+) runtime at: /usr/bin/java.
-YD_INSTALL_JAVA="${YD_INSTALL_JAVA:-TRUE}"
-
 # Set to "TRUE" for a Configured Worker Pool installation
 YD_CONFIGURED_WP="${YD_CONFIGURED_WP:-FALSE}"
 
 # Define user and directory names used by the Agent
 YD_AGENT_USER="${YD_AGENT_USER:-yd-agent}"
-YD_AGENT_ROOT="${YD_AGENT_ROOT:-/opt/yellowdog}"
 YD_AGENT_HOME="${YD_AGENT_HOME:-/opt/yellowdog/agent}"
 YD_AGENT_DATA="${YD_AGENT_DATA:-/var/opt/yellowdog/agent/data}"
 
@@ -60,37 +54,6 @@ case $DISTRO in
     exit 1
     ;;
 esac
-
-if [[ ! $(getent passwd $YD_AGENT_USER) ]]; then
-  yd_log "Creating user/group: $YD_AGENT_USER"
-  mkdir -p $YD_AGENT_ROOT
-  case $DISTRO in
-    "ubuntu" | "debian")
-      adduser $YD_AGENT_USER --home $YD_AGENT_HOME --disabled-password \
-              --quiet --gecos ""
-      ADMIN_GRP="sudo"
-      ;;
-    "almalinux" | "centos" | "rhel" | "amzn" | "fedora")
-      adduser $YD_AGENT_USER --home-dir $YD_AGENT_HOME
-      ADMIN_GRP="wheel"
-      ;;
-    "sles" | "suse")
-      if [[ ! $(getent group $YD_AGENT_USER) ]]; then
-        groupadd $YD_AGENT_USER
-      fi
-      useradd $YD_AGENT_USER --home-dir $YD_AGENT_HOME --create-home \
-              -g $YD_AGENT_USER
-      ADMIN_GRP="wheel"
-      ;;
-    *)
-      yd_log "Unknown distribution ... exiting"
-      exit 1
-      ;;
-  esac
-  yd_log "Creating Agent data directories / setting permissions"
-  mkdir -p "$YD_AGENT_DATA/actions" "$YD_AGENT_DATA/workers"
-  chown -R $YD_AGENT_USER:$YD_AGENT_USER $YD_AGENT_HOME $YD_AGENT_DATA
-fi
 
 ################################################################################
 
@@ -160,59 +123,18 @@ yd_log "Agent configuration file created"
 
 ################################################################################
 
-yd_log "Setting directory permissions"
-chown $YD_AGENT_USER:$YD_AGENT_USER -R $YD_AGENT_HOME
-chmod ug+x $YD_AGENT_HOME/start.sh
-
-################################################################################
-
-yd_log "Setting up the Agent systemd service"
-
-if [[ ! $YD_CONFIGURED_WP == "TRUE" ]]; then
-  SD_AFTER="cloud-final.service"
-  SD_WANTED_BY="cloud-init.target"
-else
-  SD_AFTER="network.target"
-  SD_WANTED_BY="multi-user.target"
+if [[ $YD_CONFIGURED_WP == "TRUE" ]]; then
+  yd-log "Reconfiguring Agent service (yd-agent) for configured worker pool"
+  sed -i "s/^After=cloud-final.service yd-promtail.service$/After=network.target/" \
+    /etc/systemd/system/yd-agent.service
+  sed -i "s/^WantedBy=cloud-init.target$/WantedBy=multi-user.target/" \
+    /etc/systemd/system/yd-agent.service
 fi
 
-cat > /etc/systemd/system/yd-agent.service << EOM
-[Unit]
-Description=YellowDog Agent
-After=$SD_AFTER
-
-[Service]
-User=$YD_AGENT_USER
-WorkingDirectory=$YD_AGENT_HOME
-ExecStart=$YD_AGENT_HOME/start.sh
-SuccessExitStatus=143
-TimeoutStopSec=10
-Restart=on-failure
-RestartSec=5
-LimitMEMLOCK=8388608
-
-[Install]
-WantedBy=$SD_WANTED_BY
-EOM
-
-mkdir -p /etc/systemd/system/yd-agent.service.d
-
-cat > /etc/systemd/system/yd-agent.service.d/yd-agent.conf << EOM
-[Service]
-Environment="YD_AGENT_HOME=$YD_AGENT_HOME"
-Environment="YD_AGENT_DATA=$YD_AGENT_DATA"
-EOM
-
-yd_log "Systemd files created"
-
-yd_log "(Stopping,) enabling & starting Agent service (yd-agent)"
-systemctl stop yd-agent &> /dev/null
-systemctl enable yd-agent &> /dev/null
+yd_log "Starting Agent service (yd-agent)"
 systemctl start --no-block yd-agent &> /dev/null
-yd_log "Agent service (stopped,) enabled and (re)started"
+yd_log "Agent service started"
 
 ################################################################################
 
 yd_log "YellowDog Agent installation complete"
-
-################################################################################
