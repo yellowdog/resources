7153f577d411d08d7841d5960a4909844246828c
pwt <1089749+pwt@users.noreply.github.com>
2025-01-22 14:24:59 +0000

    Fix logging pattern

--- agent-install/linux/yd-agent-installer.sh
+++ agent-install/linux/yd-agent-installer.sh
@@ -140,7 +140,7 @@ yda:
     enabled: "${YD_SCHEDULE_ENABLED:-false}"
     startup: ${YD_SCHEDULE_STARTUP:-['0 0 18 * * MON-FRI']}
     shutdown: ${YD_SCHEDULE_SHUTDOWN:-['0 0 7 * * MON-FRI']}
-logging.pattern.console: "%d{yyyy-MM-dd HH:mm:ss,SSS} Worker[%10.10thread]\
+logging.pattern.console: "%d{yyyy-MM-dd HH:mm:ss.SSS} Worker[%10.10thread]\
  %-5level[%40logger{40}] %message [%class{0}:%method:%line]%n"
 EOM
 fi
